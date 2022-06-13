10c5668cc59fb92923915f1248039d5349f067ff
Arturo Ciaceri <arturo.ciaceri@gmail.com>
2022-06-13 19:22:07 +0200

    feat-mock-db

---
+++ src/main/java/it/develhope/h2exercise10/H2Exercise10Application.java
@@ -0,0 +1,13 @@
+package it.develhope.h2exercise10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2Exercise10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2Exercise10Application.class, args);
+	}
+
+}
